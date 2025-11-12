b49a7184996237ab9abbc22b364066548f801780
benden1106 <denchb@dickinson.edu>
2025-11-12 12:46:34 -0500

    Opened up the file on Cypress

--- modules/farm_fd2_school/10-FD4-Tutorials-Starter/firstTest.e2e.cy.js
+++ modules/farm_fd2_school/10-FD4-Tutorials-Starter/firstTest.e2e.cy.js
@@ -1,6 +1,6 @@
 describe('FD2-Tutorial - A First Test', () => {
   it('Check the page state when loaded', () => {
     cy.visit('./index.html');
-    cy.get('h1').should('have.text', 'Shopping List App');
+    cy.get('h1').should('have.text', 'Shopping List Application');
   });
 });
